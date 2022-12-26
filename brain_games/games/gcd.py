5162b1147ff7b95b4049f189f70c5d44e8d6a972
Aleksandr Vysotskii <sanya@MacBook-Air-Aleksandr.local>
2022-12-26 16:06:46 +0400

    add step 7

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,25 @@
+from random import choices
+from brain_games.general_logic import game
+
+
+rule = 'Find the greatest common divisor of given numbers.'
+
+
+def get_gcd(num1, num2):
+    min_num, max_num = min(num1, num2), max(num1, num2)
+    divisor = min_num
+    while divisor > 0:
+        if max_num % divisor == 0 and min_num % divisor == 0:
+            return divisor
+        divisor -= 1
+
+
+def get_quiz():
+    num1, num2 = choices(range(1, 100), k=2)
+    question = num1, num2
+    answer = get_gcd(num1, num2)
+    return question, str(answer)
+
+
+def run_game():
+    game(get_quiz, rule)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,9 @@
+from brain_games.games import gcd
+
+
+def main():
+    gcd.run_game()
+
+
+if __name__ == '__main__':
+    main()
