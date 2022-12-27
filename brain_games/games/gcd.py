03dfde3fe6193335d7d1e2098ae166bb25d380c7
Aleksandr Vysotskii <sanya@MacBook-Air-Aleksandr.local>
2022-12-27 18:52:21 +0400

    correct gsd.py get_quiz

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -16,7 +16,7 @@ def get_gcd(num1, num2):
 
 def get_quiz():
     num1, num2 = choices(range(1, 100), k=2)
-    question = num1, num2
+    question = "{} {}".format(num1, num2)
     answer = get_gcd(num1, num2)
     return question, str(answer)
 
