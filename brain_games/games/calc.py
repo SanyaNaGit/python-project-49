a8c6a1d00a3129ea4892cd3a38da159d5f08be31
Aleksandr Vysotskii <sanya@MacBook-Air-Aleksandr.local>
2023-01-25 00:53:11 +0400

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,17 +5,13 @@ RULE = 'What is the result of the expression?'
 
 def get_quiz():
     sign = choice(["*", "+", "-"])
-
-    def expression(num_1, num_2):
-        if sign == "*":
-            result = num_1 * num_2
-        elif sign == "+":
-            result = num_1 + num_2
-        elif sign == "-":
-            result = num_1 - num_2
-        return result
-
     num1, num2 = choices(range(1, 100), k=2)
-    answer = expression(num1, num2)
+    if sign == "*":
+        result = num1 * num2
+    elif sign == "+":
+        result = num1 + num2
+    elif sign == "-":
+        result = num1 - num2
+    answer = result
     question = "{} {} {}".format(num1, sign, num2)
     return question, str(answer)
